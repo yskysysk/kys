ac891d9635ff2e241df456457e6e43cf95134afe
yskysysk <shialove17@gmail.com>
2025-05-22 12:41:52 +0900

    0522 up

--- js/app.js
+++ js/app.js
@@ -11,4 +11,35 @@ document.addEventListener("DOMContentLoaded",function(){
 
    })
 
+   const searchInput = document.getElementById("searchInput")
+   const portfolioItems = document.querySelectorAll(".portfolio-item")
+
+   searchInput.addEventListener("input", function(){
+        const searchTerm = this.value.toLowerCase();
+        portfolioItems.forEach((item)=>{
+           const title= item.querySelector("h4").textContent.toLowerCase();
+           if(title.includes(searchTerm)) {
+            item.style.display = "block";
+
+           } else {
+            item.style.display = "none";
+           }
+        })
+
+   })
+    const filterButtons = document.querySelectorAll(".filter-btn");
+    filterButtons.forEach((button)=> {
+        button.addEventListener("click", function(){
+           const filter = this.getAttribute("data-filter");
+           portfolioItems.forEach((item) => {
+            if(filter === 'all' || item.getAttribute("data-category") ===filter) {
+                    item.style.display = "block";
+            }else{
+                item.style.display = "none";
+            }
+           }) 
+        })
+    })
+
+
 })
